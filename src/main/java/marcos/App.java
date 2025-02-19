e2242213420c507a9cf1583e4a691c31b43cc7da
Ranjan <58366704+rjn991@users.noreply.github.com>
2025-02-19 12:33:59 +0530

    Update App.java

--- src/main/java/marcos/App.java
+++ src/main/java/marcos/App.java
@@ -5,6 +5,6 @@ package marcos;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!MF");
+        System.out.println("Hello World!MF")
     }
 }
